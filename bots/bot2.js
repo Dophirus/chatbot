a430c4afa1b9a55c4bf8ab4e761a7c7a28bb5bed
Dophirus <73464642+Dophirus@users.noreply.github.com>
2024-06-13 00:53:16 +0200

    fix anime bot

--- bots/bot2.js
+++ bots/bot2.js
@@ -1,17 +1,24 @@
 import Bot from '../bots/bot';
-import { fetchVgmdbData } from '../services/vgmdbService';
+import { fetchAnimeMangaData } from '../services/vgmdbService';
 
 class Bot2 extends Bot {
   constructor() {
     super('VGMDBbot');
     this.addCommand('hello', () => 'Hello from VGMDBbot !');
     this.addCommand('help', () => 'Available commands: hello, help, vgmdb');
-    this.addCommand('vgmdb', async (query) => {
-      const data = await fetchVgmdbData(query);
-      console.log('VGMdb data:', data);
-      return `VGMdb data for ${query}: ${data}`;
+    this.addCommand('anime', async (name) => {
+      const data = await fetchAnimeMangaData(name);
+      if (data) {
+        return `Informations sur "${name}":
+            - Titre: ${data.title}
+            - Synopsis: ${data.synopsis || 'Pas de synopsis disponible.'}
+            - Date de début: ${data.aired.from}
+            - Genres: ${data.genres.map(genre => genre.name).join(', ')}
+            - Image: ${data.images.jpg.large_image_url}`;
+      } else {
+        return 'Impossible de récupérer les informations sur cet anime/manga pour le moment.';
+      }
     });
   }
 }
-
 export default Bot2;
--- services/vgmdbService.js
+++ services/vgmdbService.js
@@ -1,13 +1,18 @@
-export async function fetchVgmdbData(query) {
+const apiUrl = 'https://api.jikan.moe/v4/anime';
+
+export async function fetchAnimeMangaData(name) {
   try {
-    const response = await fetch(`https://vgmdb.net/search?q=${query}`, {
-      mode: 'no-cors'
-    });
-    const data = await response.text();
-    console.log('VGMdb API response:', data);
-    return data;
+    const response = await fetch(`${apiUrl}?q=${encodeURIComponent(name)}`);
+    const data = await response.json();
+    if (data && data.data && data.data.length > 0) {
+      console.log('Anime/Manga data:', data.data[0]);
+      return data.data[0];
+    } else {
+      console.error('No data found for the given name.');
+      return null;
+    }
   } catch (error) {
-    console.error('Error fetching VGMdb data:', error);
-    return 'Error fetching VGMdb data';
+    console.error('Error fetching anime/manga data:', error);
+    return null;
   }
 }
